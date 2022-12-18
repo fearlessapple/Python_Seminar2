bbd439fce19ddf786fcca5e85c37a9d4039fec52
Stas <smaster950@gmail.com>
2022-12-18 10:38:09 +0300

    Complete Exercise_1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,3 +1,12 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
- 
+n = float(input("Input your number: "))
+total = 0
+if n < 0:
+    n = (-1)*n
+while n != int(n):
+    n = round(n*10, 5)
+while n != 0:
+    total += n % 10
+    n = n // 10
+print(total)    
