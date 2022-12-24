9151ad276bed0f53eace73de33550ab5a4402197
Stas <smaster950@gmail.com>
2022-12-24 16:59:05 +0300

    Upgrade Exercise_5 file

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,6 +1,17 @@
 # Реализуйте алгоритм перемешивания списка.
 
 import random
-my_list = ["264", "75959", "bvijsn", "efilbvdwboi4", "skjbv", "22245"]
-random.shuffle(my_list)
-print(my_list)
+# my_list = ["264", "75959", "bvijsn", "efilbvdwboi4", "skjbv", "22245"]
+# random.shuffle(my_list)
+# print(my_list)
+
+def list_shuffle(_list: list):
+    shuffled_list = []
+    temp_list = _list
+
+    for _ in range(len(_list)):
+        position = random.randint(0, len(temp_list)-1)
+        shuffled_list.append(temp_list.pop(position))
+    return shuffled_list
+
+print(list_shuffle([1, 2, 8, 6, 7, 12]))    
